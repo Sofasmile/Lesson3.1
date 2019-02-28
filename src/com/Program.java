d4a45c1ebc099a308ea09b4e6aaf69761499d402
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-02-28 19:08:36 +0200

    calculator

---
+++ src/com/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(sum(2,5));
+
+    }
+    public static int sum(int a, int b){
+        return a+b;
+    }
+}
