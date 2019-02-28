e27a6d9a68f132de14c8e8a7e3dca1fb91bbc729
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-02-28 19:14:04 +0200

    added difference with double numbers

--- src/com/Program.java
+++ src/com/Program.java
@@ -3,6 +3,7 @@ public class Program {
         System.out.println(sum(2,5));
         System.out.println(sum(2.3,2.6));
         System.out.println(dif(5,1));
+        System.out.println(dif(4.5,3.6));
 
     }
     public static int sum(int a, int b){
@@ -12,6 +13,9 @@ public class Program {
         return a+b;
     }
     public static int dif(int a, int b){
+        return a-b;
+    }
+    public static double dif(double a, double b){
         return a-b;
     }
 }
