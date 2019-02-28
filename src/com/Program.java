e0262a5280f7f89b4f83529b811d9ce2ce29cbcd
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-02-28 19:15:46 +0200

    added multiplication with int numbers

--- src/com/Program.java
+++ src/com/Program.java
@@ -1,9 +1,12 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println(sum(2,5));
-
+        System.out.println(mul(4,9));
     }
     public static int sum(int a, int b){
         return a+b;
+    }
+    public  static int mul(int a, int b){
+        return a*b;
     }
 }
