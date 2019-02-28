cd6f1408e98a49d71571e3e9c088858486fe1b9c
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-02-28 19:10:53 +0200

    added sum with double numbers

--- src/com/Program.java
+++ src/com/Program.java
@@ -1,9 +1,13 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println(sum(2,5));
+        System.out.println(sum(2.3,2.6));
 
     }
     public static int sum(int a, int b){
+        return a+b;
+    }
+    public  static  double sum(double a, double b){
         return a+b;
     }
 }
