b742a8502384c7b4c037aa21a343404cc43f2bcd
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-02-28 19:16:40 +0200

    added multiplication with double numbers

--- src/com/Program.java
+++ src/com/Program.java
@@ -2,11 +2,15 @@ public class Program {
     public static void main(String[] args) {
         System.out.println(sum(2,5));
         System.out.println(mul(4,9));
+        System.out.println(mul(4.5, 2.3));
     }
     public static int sum(int a, int b){
         return a+b;
     }
     public  static int mul(int a, int b){
+        return a*b;
+    }
+    public static double mul(double a, double b){
         return a*b;
     }
 }
