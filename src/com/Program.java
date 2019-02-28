5d926bb19897acc50bd82de79af510816a0a008c
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-02-28 19:12:53 +0200

    added difference with int numbers

--- src/com/Program.java
+++ src/com/Program.java
@@ -2,6 +2,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println(sum(2,5));
         System.out.println(sum(2.3,2.6));
+        System.out.println(dif(5,1));
 
     }
     public static int sum(int a, int b){
@@ -9,5 +10,8 @@ public class Program {
     }
     public  static  double sum(double a, double b){
         return a+b;
+    }
+    public static int dif(int a, int b){
+        return a-b;
     }
 }
